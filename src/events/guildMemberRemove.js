16dd7b1c3015d78a3949d1ea2c7290d90eb140f6
Expo <dsidari25@gmail.com>
2023-06-23 17:51:47 -0700

    Remove unnecessary references
    guildMemberRemove.js

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, GuildAuditLogs, GuildAuditLogsEntry } = require('discord.js');
 
 module.exports = {
     name: 'guildMemberRemove',
